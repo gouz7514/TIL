c76cccbcf2d247d0f9b91909a5bdc41b64a57f16
gouz7514 <gouz7514@gmail.com>
2021-03-03 16:27:41 +0900

    update 프로그래머 연습 - 프린터.js

---
+++ 코딩 연습/프로그래머스/프린터.js
@@ -0,0 +1,22 @@
+// 문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/42587
+function solution(priorities, location) {
+  var arr = priorities.map((priority, index) => {
+      return {
+          index: index, priority: priority
+      };
+  });
+
+  var queue = [];
+
+  while(arr.length > 0) {
+      var firstEle = arr.shift();
+      var hasHighPriority = arr.some(ele => ele.priority > firstEle.priority);
+      if (hasHighPriority) {
+          arr.push(firstEle);
+      } else {
+          queue.push(firstEle);
+      }
+  }
+
+  return queue.findIndex(queueEle => queueEle.index === location) + 1;
+}
