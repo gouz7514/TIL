012258c843b7065b100017c862cc5554891a3d05
gouz7514 <gouz7514@gmail.com>
2021-06-09 10:06:58 +0900

    update findOdd.js

---
+++ 코딩 연습/Codewars/findOdd.js
@@ -0,0 +1,39 @@
+// my
+// object iterate 할 때는 for ... in obj 사용
+function findOdd(A) {
+  let obj = {};
+  for (let i of A) {
+    if (!obj[i]) {
+      obj[i] = 1;
+    } else {
+      obj[i] += 1;
+    }
+  }
+
+  for (let o in obj) {
+    if (obj[o] % 2 !== 0) {
+      return o;
+    }
+  }
+}
+
+let A = [1,1,2,-2,5,2,4,4,-1,-2,5]
+let res = findOdd(A);
+console.log(parseInt(res));
+
+// 위 함수 줄이기
+function findOdd(A) {
+  let obj = {};
+  A.forEach((el) => {
+    obj[el] ? obj[el]++ : obj[el] = 1;
+  });
+
+  for (prop in obj) {
+    if (obj[prop] % 2 != 0) return Number(prop);
+  }
+}
+
+// clever
+function findOdd(A) {
+  return A.reduce((a,b) => a ^ b);
+}
