2515c03f6f64e3450fddff14f859076ee04bc5c7
HOTJAE <gouz7514@naver.com>
2021-08-02 10:48:21 +0900

    update : understand export default ~

--- 자바스크립트/exportPractice/main.js
+++ 자바스크립트/exportPractice/main.js
@@ -1,11 +1,20 @@
-import { thing as importedThing } from './module.js';
-const module = await import('./module.js');
-let { thing } = await import('./module.js');
+// import { thing as importedThing } from './module.js';
+// const module = await import('./module.js');
+// let { thing } = await import('./module.js');
 
-setTimeout(() => {
-  console.log(importedThing); // changed
-  console.log(module.thing); // changed
-  console.log(thing); // initial
-}, 1000);
+// setTimeout(() => {
+//   console.log(importedThing); // changed
+//   console.log(module.thing); // changed
+//   console.log(thing); // initial
+// }, 1000);
 
 // destructured import는 새로운 식별자에 현재 값을 할당하기 때문에 변화 반영 X
+
+import { thing, default as defaultThing } from './module.js'
+import anotherDefaultThing from './module.js';
+
+setTimeout(() => {
+  console.log(thing); // changed
+  console.log(defaultThing); // initial
+  console.log(anotherDefaultThing); // initial
+}, 1000);
--- 자바스크립트/exportPractice/module.js
+++ 자바스크립트/exportPractice/module.js
@@ -1,4 +1,15 @@
-export let thing = 'initial';
+// export let thing = 'initial';
+
+// setTimeout(() => {
+//   thing = 'changed';
+// }, 500);
+
+let thing = 'initial';
+
+export { thing };
+export default thing;
+// export default의 경우 뒤 구문을 exporession으로 처리해서 값에 의한 전달 발생
+// 즉, export 되기 바로 전 일종의 숨어있는 변수에 할당되서 변화가 export 시에 반영되지 않음
 
 setTimeout(() => {
   thing = 'changed';
